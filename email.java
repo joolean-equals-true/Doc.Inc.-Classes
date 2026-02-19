0f26ead8547c6144509f618477d5141a4dc45c0a
joolean-equals-true <Julian.Moore7970@gmail.com>
2026-02-19 15:12:17 -0600

    two checkstyle changes to Email.java

--- email.java
+++ email.java
@@ -4,31 +4,30 @@ import java.time.LocalDate;
  * Email class for managing email-related information.
  */
 public class Email {
-    public String name;
-    public Integer document_id;
-    public Integer business_id;
+  public String name;
+  public Integer documentId;
+  public Integer businessId;
 
-    public LocalDate expiration_date;
-    public Boolean is_visible;
-    public Integer id;
+  public LocalDate expirationDate;
+  public Boolean isVisible;
+  public Integer id;
 
+  Email() {
+    this.name = "";
+    this.documentId = null;
+    this.businessId = null;
+    this.expirationDate = null;
+    this.isVisible = false;
+    this.id = null;
+  }
 
-    Email(){
-        this.name = "";
-        this.document_id = null;
-        this.business_id = null;
-        this.expiration_date = null;
-        this.is_visible = false;
-        this.id = null;
-    }
-
-      Email(String name, Integer documentId, Integer businessId,
-          LocalDate expirationDate, Boolean isVisible, Integer id) {
-        this.name = name;
-        this.document_id = documentId;
-        this.business_id = businessId;
-        this.expiration_date = expirationDate;
-        this.is_visible = isVisible;
-        this.id = id;
-      }
+  Email(String name, Integer documentId, Integer businessId,
+      LocalDate expirationDate, Boolean isVisible, Integer id) {
+    this.name = name;
+    this.documentId = documentId;
+    this.businessId = businessId;
+    this.expirationDate = expirationDate;
+    this.isVisible = isVisible;
+    this.id = id;
+  }
 }
